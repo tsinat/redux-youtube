9b2798c51d4a3f5ec903a1209d4794fb40f0acd5
Tsinat Zeree <cheru87@gmail.com>
2016-09-03 16:50:44 -0700

    fetching from youtube api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
- import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app.js'
+import SearchBar from './components/search_bar.js';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+
+const API_KEY = 'AIzaSyAjtBW_oOzYKKCI6iGs4kLUGO2fpvsDDBM';
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+    render(){
+        return (
+            <div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
     <App />
