01a5ba1da9c3fd996d5042c7300075957968e378
Tsinat Zeree <cheru87@gmail.com>
2016-09-04 14:58:31 -0700

    videos list

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+    const VideoItems =  props.videos.map((video) => {
+        return <VideoListItem video={video} key={video.etag}/>
+    });
+
     return (
         <ul className='col-md-4 list-group'>
-            {props.videos.length}
+            { VideoItems }
         </ul>
     );
 };
